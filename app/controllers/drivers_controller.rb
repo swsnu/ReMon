afadee8dd3a8dfdbbf0a3741666c21db2a5e3b25
Daegeun Lee <elnn@elnn.kr>
2014-11-02 19:24:38 +0900

    Implement driver controller for API v1.1

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,5 +1,15 @@
 class DriversController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
+  def info
+    data = {
+      :application_id => "app_0",
+      :application_name => "HelloREEF",
+      :support_aggregation => true,
+      :resource_names => [{:resource => "memory"}, {:resource => "cpu"}],
+    }
+    render :json => data
+  end
 
   def control
     data = {:success => true, :result => "SUCCESS"}
