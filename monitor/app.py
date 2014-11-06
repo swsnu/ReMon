803ce04c67f84f853e2ee370c57a5c8ec5df1662
Daegeun Lee <elnn@elnn.kr>
2014-11-06 21:03:28 +0900

    Change to one page random demo

--- monitor/app.py
+++ monitor/app.py
@@ -8,7 +8,8 @@ import tornado.httpserver
 import tornado.web
 from tornado.options import define, options
 
-define('port', default=8080, help='run on the given port', type=int)
+define('port', default=8000, help='run on the given port', type=int)
+
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -17,15 +18,6 @@ class Application(tornado.web.Application):
         }
         handlers = [
             (r'/', MainHandler),
-            (r'/api/', ApplicationHandler),
-            (r'/api/evaluators', EvaluatorListHandler),
-            (r'/api/evaluators/(\w+)/status', EvaluatorStatusHandler),
-            (r'/api/evaluators/(\w+)/resources/(\w+)', EvaluatorResourceHandler),
-            (r'/api/evaluators/(\w+)/timeseries/(\w+)', EvaluatorTimeseriesHandler),
-            (r'/api/tasks', TaskListHandler),
-            (r'/api/tasks/(\w+)/status', TaskStatusHandler),
-            (r'/api/tasks/(\w+)/logs', TaskLogsHandler),
-            (r'/api/control', ControlHandler),
         ]
         tornado.web.Application.__init__(self, handlers, **settings)
 
@@ -37,106 +29,6 @@ class BaseHandler(tornado.web.RequestHandler):
 class MainHandler(BaseHandler):
     def get(self):
         self.render('index.html')
-
-
-class ApplicationHandler(BaseHandler):
-    def get(self):
-        data = {
-            'application_id': 'app_0',
-            'application_name': 'HelloREEF',
-            'support_aggregation': True,
-            'resource_names': [{'resource': 'memory'}, {'resource': 'cpu'}],
-        }
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class EvaluatorListHandler(BaseHandler):
-    def get(self):
-        data = {'evaluators': [{'evaluator_id': 'ev_%d' % i} for i in xrange(5)]}
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class EvaluatorStatusHandler(BaseHandler):
-    def get(self, evaluator_id):
-        task_id = 'task_' + evaluator_id
-        data = {'evaluator_id': evaluator_id, 'status': 'AVAILABLE', 'assigned_task_id': task_id}
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class EvaluatorResourceHandler(BaseHandler):
-    def get(self, evaluator_id, resource_name):
-        current_time = int(time.time())
-        point = {'time': current_time, 'value': random.random()}
-        data = {
-            'evaluator_id': evaluator_id,
-            'resource': resource_name,
-            'point': point,
-        }
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class EvaluatorTimeseriesHandler(BaseHandler):
-    def get(self, evaluator_id, resource_name):
-        current_time = int(time.time())
-        points = [{'time': current_time + 1000 * t, 'value': random.random()} \
-                    for t in xrange(-19, 0)]
-        data = {
-            'evaluator_id': evaluator_id,
-            'resource': resource_name,
-            'points': points,
-        }
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class TaskListHandler(BaseHandler):
-    def get(self):
-        data = {'tasks': [{'id': 'task_ev_%d' % i} for i in xrange(5)]}
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class TaskStatusHandler(BaseHandler):
-    def get(self, task_id):
-        data = {'task_id': task_id, 'status': 'RUNNING'}
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class TaskLogsHandler(BaseHandler):
-    def get(self, task_id):
-        current_time = int(time.time())
-        logs = [{
-            'time': current_time - 20000,
-            'message': 'Hello, world!',
-            'source': task_id[5:],
-            'level': 'DEBUG',
-            'tags': [{'tag_name': 'Hello'}, {'tag_name': 'Test'}],
-        }, {
-            'time': current_time - 10000,
-            'message': 'Nice to meet you.',
-            'source': task_id[5:],
-            'level': 'DEBUG',
-            'tags': [{'tag_name': 'Test'}],
-        }]
-        data = {
-            'task_id': task_id,
-            'logs': logs,
-        }
-        json = tornado.escape.json_encode(data)
-        self.write(json)
-
-
-class ControlHandler(BaseHandler):
-    def post(self):
-        command = self.get_argument('command')
-        data = {'result': 'helloworld'}
-        json = tornado.escape.json_encode(data)
-        self.write(json)
 
 
 if __name__ == '__main__':
--- monitor/static/js/demo.js
+++ monitor/static/js/demo.js
@@ -1,29 +1,28 @@
 $(function() {
-
-    var tv = 250;
-
-    var graph = new Rickshaw.Graph( {
-        element: document.getElementById('chart'),
-        width: 600,
-        height: 400,
-        renderer: 'line',
-        series: new Rickshaw.Series.FixedDuration([{ name: 'one' }], undefined, {
-            timeInterval: tv,
-            maxDataPoints: 100,
-            timeBase: new Date().getTime() / 1000
-        }) 
-    } );
-
-    graph.render();
-
+    var graph = [];
+    var ngraphs = 6;
+    var tv = 500;
+    for (var j = 0; j < ngraphs; ++j) {
+        var element = document.getElementById('chart' + j);
+        graph[j] = new Rickshaw.Graph({
+            element: element,
+            width: element.offsetWidth,
+            height: element.offsetWidth * 0.6,
+            renderer: 'line',
+            series: new Rickshaw.Series.FixedDuration([{ name: 'one' }], undefined, {
+                timeInterval: tv,
+                maxDataPoints: 100,
+                timeBase: new Date().getTime() / 1000
+            }) 
+        });
+    }
     var i = 0;
-    var iv = setInterval( function() {
-        
-        var data = { one: (Math.sin(i++ / 40) + 4) * (Math.floor(Math.random()*100) + 400) };
-
-        graph.series.addData(data);
-        graph.render();
-
-    }, tv );
-
+    var iv = setInterval(function() {
+        for (var j = 0; j < ngraphs; ++j) {
+            var data = { one: (Math.sin(i++ / 40) + 4) * (Math.floor(Math.random()*100) + 400) };
+            graph[j].series.addData(data);
+            graph[j].render();
+            document.getElementById('value' + j).innerHTML = data.one;
+        }
+    }, tv);
 });
