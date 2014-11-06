ae868f7347ef6eb44a930ea8d7da4171b2bb6863
Daegeun Lee <elnn@elnn.kr>
2014-11-06 21:59:59 +0900

    Implement websocket echo server

--- monitor/app.py
+++ monitor/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 import os.path
 import random
 import time
@@ -6,6 +7,7 @@ import tornado.escape
 import tornado.ioloop
 import tornado.httpserver
 import tornado.web
+import tornado.websocket
 from tornado.options import define, options
 
 define('port', default=8000, help='run on the given port', type=int)
@@ -18,6 +20,7 @@ class Application(tornado.web.Application):
         }
         handlers = [
             (r'/', MainHandler),
+            (r'/websocket', WebsocketHandler),
         ]
         tornado.web.Application.__init__(self, handlers, **settings)
 
@@ -31,7 +34,41 @@ class MainHandler(BaseHandler):
         self.render('index.html')
 
 
+class MessageQueue:
+    def __init__(self):
+        self.subscribers = set()
+
+    def register(self, client):
+        self.subscribers.add(client)
+
+    def unregister(self, client):
+        self.subscribers.remove(client)
+
+    def publish(self, message):
+        logging.info('Publishing "%s"' % message)
+        for client in self.subscribers:
+            client.write_message(message)
+
+
+class WebsocketHandler(tornado.websocket.WebSocketHandler):
+    def __init__(self, *args, **kwargs):
+        self.mq = MessageQueue()
+        tornado.websocket.WebSocketHandler.__init__(self, *args, **kwargs)
+
+    def open(self):
+        logging.info('Websocket opened')
+        self.mq.register(self)
+
+    def on_close(self):
+        logging.info('Websocket closed')
+        self.mq.unregister(self)
+
+    def on_message(self, message):
+        self.mq.publish('Echo: %s' % message)
+
+
 if __name__ == '__main__':
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO)
     tornado.options.parse_command_line()
     httpserver = tornado.httpserver.HTTPServer(Application())
     httpserver.listen(options.port)
