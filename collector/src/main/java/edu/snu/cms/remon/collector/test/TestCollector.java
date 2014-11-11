8170d54bfd85561e0574eba0c1bce84530ee7b6b
TaeHunKim <rapaellk@snu.ac.kr>
2014-11-11 16:24:56 +0900

    Merge two test files into 1 file

--- collector/src/main/java/edu/snu/cms/remon/collector/test/TestCollector.java
+++ collector/src/main/java/edu/snu/cms/remon/collector/test/TestCollector.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+import org.apache.reef.task.TaskMessage;
+import org.apache.reef.util.Optional;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.snu.cms.remon.collector.Codec;
 import edu.snu.cms.remon.collector.Collector;
 
 /*
@@ -22,6 +25,7 @@ public class TestCollector {
 
 	@After
 	public void tearDown() throws Exception {
+		Collector.values.clear();
 	}
 
 	@Test
@@ -32,6 +36,16 @@ public class TestCollector {
 			Collector.putData("TestTag" + i, 0.5 +i);
 		}
 		assertEquals(0, Collector.values.size());
+	}
+	
+	@Test
+	public void testGetMessage() { // test for Collector.heartbeatHandler.getMessage()
+		Collector.heartbeatHandler hbHandler = new Collector.heartbeatHandler();
+		for (int i = 0; i < 30; i++) {
+			Collector.putData("TestTag" + i, 0.5 +i);
+		}
+		Optional<TaskMessage> op = hbHandler.getMessage();
+		assertArrayEquals(op.get().get(), new Codec().encode(Collector.values));
 	}
 
 }
