9ef857a47922fa9d91e77c63e2f0f3e2ef1bd536
Daegeun Lee <elnn@elnn.kr>
2014-11-13 22:38:37 +0900

    Test code for MainHandler

--- monitor/tests/test_monitor.py
+++ monitor/tests/test_monitor.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
-import tornado.escape
-import tornado.ioloop
-import tornado.web
-import tornado.websocket
+from tornado import gen
+from tornado.escape import json_encode
+from tornado.ioloop import TimeoutError
+from tornado.websocket import websocket_connect
 from tornado.testing import AsyncHTTPTestCase, gen_test
 
-import app
+from app import Application
 
 
-class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
-
-    def on_message(self, message):
-        self.write_message(message)
-
-
-class WebSocketTest(AsyncHTTPTestCase):
-
+class MainHandlerTest(AsyncHTTPTestCase):
+    
     def get_app(self):
-        app = tornado.web.Application([('/', EchoWebSocketHandler)])
-        return app
+        return Application()
 
-    @gen_test
-    def test_echo(self):
-        address = 'ws://localhost:%d/' % self.get_http_port()
-        ws = yield tornado.websocket.websocket_connect(address, io_loop=self.io_loop)
-        ws.write_message(tornado.escape.json_encode({'register': 'cpu'}))
-        response = yield ws.read_message()
-        self.assertIn(tornado.escape.json_encode({'register': 'cpu'}), response,
-                      "Server didn't send source not a_response. Instead sent: %s" % response)
+    def test_main_page(self):
+        response = self.fetch(r'/')
+        assert(response.code == 200)
+
+    def test_not_found(self):
+        response = self.fetch(r'/i-am-lost')
+        assert(response.code == 404)
+
+    def test_static_file(self):
+        response = self.fetch(r'/static/js/remon.js')
+        assert(response.code == 200)
+
