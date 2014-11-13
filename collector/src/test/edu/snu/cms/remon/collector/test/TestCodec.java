a97c3909ffe834b8a276d6e8a6d79ff9dfb503d1
TaeHunKim <rapaellk@snu.ac.kr>
2014-11-13 17:44:38 +0900

    Change indentation style

--- collector/src/test/edu/snu/cms/remon/collector/test/TestCodec.java
+++ collector/src/test/edu/snu/cms/remon/collector/test/TestCodec.java
@@ -14,35 +14,34 @@ import edu.snu.cms.remon.collector.Codec;
 import edu.snu.cms.remon.collector.Collector;
 
 /**
- * Unit test for Codec class 
+ * Unit test for Codec class
  */
 public class TestCodec {
-	
-	@Before
-	public void setUp() throws Exception {
-	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+  @Before
+  public void setUp() throws Exception {
+  }
 
-	/**
-	 * Unit test for Codec.encode() and Codec.decode()
-	 * Encode and decode a array and
-	 * test that decoded array is same to original array 
-	 */
-	@Test
-	public void testEncodeDecode() {
-		byte[] bArray;
-		List<Metric> values;
-		
-		values = new ArrayList<>();
-		for (int i = 0; i < 20; i++) {
-			values.add(new Metric("Test"+i, 0.5+1));
-		}
-		bArray = new Codec().encode(values);
-		List<Metric> decodedvalues = new Codec().decode(bArray);
-		assertEquals(decodedvalues, values);
-	}
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Unit test for Codec.encode() and Codec.decode() Encode and decode a array
+   * and test that decoded array is same to original array
+   */
+  @Test
+  public void testEncodeDecode() {
+    byte[] bArray;
+    List<Metric> values;
+
+    values = new ArrayList<>();
+    for (int i = 0; i < 20; i++) {
+      values.add(new Metric("Test" + i, 0.5 + 1));
+    }
+    bArray = new Codec().encode(values);
+    List<Metric> decodedvalues = new Codec().decode(bArray);
+    assertEquals(decodedvalues, values);
+  }
 
 }
--- collector/src/test/edu/snu/cms/remon/collector/test/TestCollector.java
+++ collector/src/test/edu/snu/cms/remon/collector/test/TestCollector.java
@@ -15,44 +15,44 @@ import edu.snu.cms.remon.collector.Codec;
 import edu.snu.cms.remon.collector.Collector;
 
 /**
- * Unit test for Collector class 
+ * Unit test for Collector class
  */
 public class TestCollector {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+  @Before
+  public void setUp() throws Exception {
+  }
 
-	@After
-	public void tearDown() throws Exception {
-		Collector.values.clear();
-	}
+  @After
+  public void tearDown() throws Exception {
+    Collector.values.clear();
+  }
 
-	/**
-	 * Unit test for Collector.putData()
-	 * Test the size of Collector.values after call Collector.putData 
-	 */
-	@Test
-	public void testPutData() {
-		Collector.putData("TestTag", 0.5);
-		assertEquals(1, Collector.values.size());
-		for (int i = 0; i < 30; i++) {
-			Collector.putData("TestTag" + i, 0.5 +i);
-		}
-		assertEquals(0, Collector.values.size());
-	}
-	
-	/**
-	 * Unit test for Collector.heartbeatHandler.GetMessage()
-	 * Test that getMessage() encode a item in Collector.values correctly
-	 */
-	@Test
-	public void testGetMessage() {
-		Collector.heartbeatHandler hbHandler = new Collector.heartbeatHandler();
-		for (int i = 0; i < 30; i++) {
-			Collector.putData("TestTag" + i, 0.5 +i);
-		}
-		Optional<TaskMessage> op = hbHandler.getMessage();
-		assertArrayEquals(op.get().get(), new Codec().encode(Collector.values));
-	}
+  /**
+   * Unit test for Collector.putData() Test the size of Collector.values after
+   * call Collector.putData
+   */
+  @Test
+  public void testPutData() {
+    Collector.putData("TestTag", 0.5);
+    assertEquals(1, Collector.values.size());
+    for (int i = 0; i < 30; i++) {
+      Collector.putData("TestTag" + i, 0.5 + i);
+    }
+    assertEquals(0, Collector.values.size());
+  }
+
+  /**
+   * Unit test for Collector.heartbeatHandler.GetMessage() Test that
+   * getMessage() encode a item in Collector.values correctly
+   */
+  @Test
+  public void testGetMessage() {
+    Collector.heartbeatHandler hbHandler = new Collector.heartbeatHandler();
+    for (int i = 0; i < 30; i++) {
+      Collector.putData("TestTag" + i, 0.5 + i);
+    }
+    Optional<TaskMessage> op = hbHandler.getMessage();
+    assertArrayEquals(op.get().get(), new Codec().encode(Collector.values));
+  }
 }
