97ae34382cd643a2168bc73ba2a0de36298bfb68
TaeHunKim <rapaellk@snu.ac.kr>
2014-11-13 18:50:33 +0900

    Edit code to python convention style

--- monitor/tests/test_monitor.py
+++ monitor/tests/test_monitor.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
-from functools import partial
-from tornado.testing import gen_test
-from tornado import testing
-import tornado.websocket
-import tornado.web
+import tornado.escape
 import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from tornado.testing import AsyncHTTPTestCase, gen_test
+
 import app
 
 
 class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
+
     def on_message(self, message):
-      self.write_message(message)
+        self.write_message(message)
 
 
-class WebSocketTest(testing.AsyncHTTPTestCase):
+class WebSocketTest(AsyncHTTPTestCase):
 
     def get_app(self):
-      app = tornado.web.Application([('/', EchoWebSocketHandler)])
-      return app
-    
+        app = tornado.web.Application([('/', EchoWebSocketHandler)])
+        return app
+
     @gen_test
     def test_echo(self):
-      ws = yield tornado.websocket.websocket_connect('ws://localhost:%d/' % self.get_http_port(),io_loop=self.io_loop)
-      ws.write_message(str({'register': 'cpu'}))
-      response = yield ws.read_message()
-      self.assertIn(str({'register': 'cpu'}), response, "Server didn't send source not a_response. Instead sent: %s" % response)
+        address = 'ws://localhost:%d/' % self.get_http_port()
+        ws = yield tornado.websocket.websocket_connect(address, io_loop=self.io_loop)
+        ws.write_message(tornado.escape.json_encode({'register': 'cpu'}))
+        response = yield ws.read_message()
+        self.assertIn(tornado.escape.json_encode({'register': 'cpu'}), response,
+                      "Server didn't send source not a_response. Instead sent: %s" % response)
