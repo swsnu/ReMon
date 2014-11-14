9570b5e5d52d2725900f11c2b91d3f6b18ae0537
Yunseong Lee <yunseong.lee@me.com>
2014-11-14 14:44:48 +0900

    Add synchronization to avoid the concurrent access

--- collector/src/main/java/edu/snu/cms/remon/collector/MetricSerializer.java
+++ collector/src/main/java/edu/snu/cms/remon/collector/MetricSerializer.java
@@ -12,7 +12,7 @@ import java.io.OutputStream;
  * Serialize/deserialize the Metrics object from/to JSON String
  */
 public class MetricSerializer {
-  public static void toStream(Metrics metrics, OutputStream out) throws IOException {
+  public synchronized static void toStream(Metrics metrics, OutputStream out) throws IOException {
     final DatumWriter<Metrics> rulesWriter = new SpecificDatumWriter<>(Metrics.class);
     final JsonEncoder encoder = EncoderFactory.get().jsonEncoder(Metrics.SCHEMA$, out);
     rulesWriter.write(metrics, encoder);
@@ -23,7 +23,7 @@ public class MetricSerializer {
   /**
    * Parse Rules from a JSON text InputStream
    */
-  public static Metrics fromStream(InputStream in) throws IOException {
+  public synchronized static Metrics fromStream(InputStream in) throws IOException {
     final DatumReader<Metrics> reader = new SpecificDatumReader<>(Metrics.class);
     final JsonDecoder decoder = DecoderFactory.get().jsonDecoder(Metrics.getClassSchema(), in);
     return reader.read(null, decoder);
