f75ebe61c7615b1f028f08c146803bddf43a220b
TaeHunKim <rapaellk@snu.ac.kr>
2014-12-04 20:00:25 +0900

    Add cobertura coverage test for collector

--- scripts/unittest.py
+++ scripts/unittest.py
@@ -1,28 +1,44 @@
 import os
 import subprocess
 import sys
+import webbrowser
 from optparse import OptionParser
 
 usage = "usage : %prog [options] "
 parser = OptionParser(usage=usage)
-parser.add_option("-n","--no-coverage",dest="coverage",action="store_false",help="don't run coverage test",default=True)
+parser.add_option("-v","--no-coverage",dest="coverage",action="store_false",help="don't run coverage test",default=True)
+parser.add_option("-c","--collector",dest="collector",action="store_true",help="run collector test",default=False)
+parser.add_option("-m","--monitor",dest="monitor",action="store_true",help="run monitor test",default=False)
+parser.add_option("-a","--all",dest="alltest",action="store_true",help="run collector and monitor test",default=False)
 
-(option,args) = parser.parse_args()
+(options,args) = parser.parse_args()
 
-if len(args) > 1:
+if not (options.collector or options.monitor or options.alltest):
   parser.print_help()
   sys.exit(2)
 
-os.chdir('../monitor')
 
-ret = 0
+monitor_test = 0
+collector_test = 0
 
-if option.coverage:
-  ret = subprocess.call(['coverage', 'run', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
-  if ret == 0:
-    subprocess.call(['coverage', 'report', '-m', '--include=app*'])
+if options.collector:
+  os.chdir('../collector')
+  print "##### Collector Unit Test #####"
+  if options.coverage:
+    monitor = subprocess.call(['mvn', 'cobertura:cobertura'])
+    webbrowser.open('target/site/cobertura/index.html') 
+  else :
+    monitor = subprocess.call(['mvn', 'test'])
 
-else :
-  ret = subprocess.call(['python', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+if options.monitor:
+  os.chdir('../monitor')
+  print "##### Monitor Unit Test #####"
+  if options.coverage:
+    monitor_test = subprocess.call(['coverage', 'run', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+    if  monitor_test == 0:
+      subprocess.call(['coverage', 'report', '-m', '--include=app*'])
 
-sys.exit(ret)
+  else :
+    monitor_test = subprocess.call(['python', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+
+sys.exit(monitor_test | collector_test)
