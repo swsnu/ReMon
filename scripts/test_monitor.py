4194c5987d5b439a6869116bf72dcad8177c8d9f
TaeHunKim <rapaellk@snu.ac.kr>
2014-12-04 18:27:17 +0900

    Change method to run command and add coverage command

--- scripts/test_monitor.py
+++ scripts/test_monitor.py
@@ -1,4 +1,12 @@
 import os
+import subprocess
+import sys
 
 os.chdir('../monitor')
-os.system('python -m tornado.testing --verbose tests.runtests')
+ret = subprocess.call(['python', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+
+if ret == 0:
+  subprocess.call(['coverage', 'run', '-m', 'tornado.testing', '--verbose', 'tests.runtests'], stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))
+  subprocess.call(['coverage', 'report', '-m', '--include=app*'])
+
+sys.exit(ret)
