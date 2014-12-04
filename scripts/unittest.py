dd0c4905d553557bdfd651c6f3857331ee10cb0f
TaeHunKim <rapaellk@snu.ac.kr>
2014-12-04 20:10:55 +0900

    Code convention

--- scripts/unittest.py
+++ scripts/unittest.py
@@ -6,39 +6,65 @@ from optparse import OptionParser
 
 usage = "usage : %prog [options] "
 parser = OptionParser(usage=usage)
-parser.add_option("-v","--no-coverage",dest="coverage",action="store_false",help="don't run coverage test",default=True)
-parser.add_option("-c","--collector",dest="collector",action="store_true",help="run collector test",default=False)
-parser.add_option("-m","--monitor",dest="monitor",action="store_true",help="run monitor test",default=False)
-parser.add_option("-a","--all",dest="alltest",action="store_true",help="run collector and monitor test",default=False)
+parser.add_option(
+    "-v",
+    "--no-coverage",
+    dest="coverage",
+    action="store_false",
+    help="don't run coverage test",
+    default=True)
+parser.add_option(
+    "-c",
+    "--collector",
+    dest="collector",
+    action="store_true",
+    help="run collector test",
+    default=False)
+parser.add_option(
+    "-m",
+    "--monitor",
+    dest="monitor",
+    action="store_true",
+    help="run monitor test",
+    default=False)
+parser.add_option(
+    "-a",
+    "--all",
+    dest="alltest",
+    action="store_true",
+    help="run collector and monitor test",
+    default=False)
 
-(options,args) = parser.parse_args()
+(options, args) = parser.parse_args()
 
 if not (options.collector or options.monitor or options.alltest):
-  parser.print_help()
-  sys.exit(2)
+    parser.print_help()
+    sys.exit(2)
 
 
 monitor_test = 0
 collector_test = 0
 
 if options.collector:
-  os.chdir('../collector')
-  print "##### Collector Unit Test #####"
-  if options.coverage:
-    monitor = subprocess.call(['mvn', 'cobertura:cobertura'])
-    webbrowser.open('target/site/cobertura/index.html') 
-  else :
-    monitor = subprocess.call(['mvn', 'test'])
+    os.chdir('../collector')
+    print "##### Collector Unit Test #####"
+    if options.coverage:
+        monitor = subprocess.call(['mvn', 'cobertura:cobertura'])
+        webbrowser.open('target/site/cobertura/index.html')
+    else:
+        monitor = subprocess.call(['mvn', 'test'])
 
 if options.monitor:
-  os.chdir('../monitor')
-  print "##### Monitor Unit Test #####"
-  if options.coverage:
-    monitor_test = subprocess.call(['coverage', 'run', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
-    if  monitor_test == 0:
-      subprocess.call(['coverage', 'report', '-m', '--include=app*'])
+    os.chdir('../monitor')
+    print "##### Monitor Unit Test #####"
+    if options.coverage:
+        monitor_test = subprocess.call(
+            ['coverage', 'run', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+        if monitor_test == 0:
+            subprocess.call(['coverage', 'report', '-m', '--include=app*'])
 
-  else :
-    monitor_test = subprocess.call(['python', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
+    else:
+        monitor_test = subprocess.call(
+            ['python', '-m', 'tornado.testing', '--verbose', 'tests.runtests'])
 
 sys.exit(monitor_test | collector_test)
